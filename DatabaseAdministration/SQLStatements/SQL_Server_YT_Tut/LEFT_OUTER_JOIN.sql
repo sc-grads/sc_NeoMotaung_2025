b60f713c14ac3905d066a1cc5d359839be9750f4
NeoMotaung <neo.motaung@sambeconsulting.com>
2025-02-26 10:40:50 +0200

    Update LEFT_OUTER_JOIN.sql
    Trailing white space fix

--- DatabaseAdministration/SQLStatements/SQL_Server_YT_Tut/LEFT_OUTER_JOIN.sql
+++ DatabaseAdministration/SQLStatements/SQL_Server_YT_Tut/LEFT_OUTER_JOIN.sql
@@ -1,6 +1,6 @@
 --LEFT OUTER JOIN with tables stores and staffs with the store_id
 
-SELECT TOP (1000) 
+SELECT TOP (1000)
 	*
   FROM [BikeStores].[sales].[stores] s
   LEFT OUTER JOIN [BikeStores].[sales].[staffs] st
