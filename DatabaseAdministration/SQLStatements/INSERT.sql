ca598ae072a48bf032a04b3871b00cf4ecc25d82
NeoMotaung <neo.motaung@sambeconsulting.com>
2025-02-26 10:33:29 +0200

    Update INSERT.sql
    Trailing white space fix

--- DatabaseAdministration/SQLStatements/INSERT.sql
+++ DatabaseAdministration/SQLStatements/INSERT.sql
@@ -2,7 +2,7 @@
 --Table: People
 --Testing INSERT operation with table People using my details
 
-INSERT INTO People (PersonID, FirstName, LastName, Age) 
+INSERT INTO People (PersonID, FirstName, LastName, Age)
 VALUES (1, 'Neo', 'Motaung', 22);
 
 SELECT * FROM People;
