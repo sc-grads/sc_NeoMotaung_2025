b95f568f510f41025e684363ded4414fc3e23917
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-03-11 14:21:27 +0200

    Added Queries to create table personalInfo and insert a record

---
+++ DatabaseAdministration/SQLStatements/SQLAdminUdemy/INSERT_personalInfo.sql
@@ -0,0 +1,16 @@
+USE [MyFirstDB]
+GO
+
+INSERT INTO [dbo].[personalInfo]
+           ([ID]
+           ,[FirstName]
+           ,[LastName]
+           ,[DOB])
+     VALUES
+           (01
+           ,'John'
+           ,'Smith'
+           ,'01/01/2001')
+GO
+
+
