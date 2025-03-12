ccc29087532c5afb44d552e615297deafcceddf7
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-03-12 14:48:55 +0200

    Queries for creating tables courseinfo and studentinfo, testing the types of joins and sub queries

---
+++ DatabaseAdministration/SQLStatements/SQLAdminUdemy/CreateTableCourseInfo.sql
@@ -0,0 +1,11 @@
+USE [MyFirstDB]
+GO
+
+--Create table CourseInfo in MyFirstDB
+
+CREATE TABLE CourseInfo(
+	CourseID varchar(50) NOT NULL,
+	RollNo int
+) 
+
+
---
+++ DatabaseAdministration/SQLStatements/SQLAdminUdemy/CreateTableStudentInfo.sql
@@ -0,0 +1,13 @@
+USE [MyFirstDB]
+GO
+
+--Create table StudentInfo
+
+CREATE TABLE StudentInfo(
+	RollNo int NOT NULL,
+	StudentName nvarchar(50) NULL,
+	StudentAge int NULL,
+	StudentPhoneNumber varchar(50) NULL,
+	StudentCity varchar(50) NULL
+) 
+
