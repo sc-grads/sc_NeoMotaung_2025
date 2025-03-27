a369495f5fb023d4909af6f891af1b72d2c3396c
NeoMotaung <neo.motaung@sambeconsulting.com>
2025-03-27 11:19:35 +0200

    Update DBAutoTestMzu.sql

--- DatabaseAdministration/SQLStatements/Automate/DBAutoTestMzu.sql
+++ DatabaseAdministration/SQLStatements/Automate/DBAutoTestMzu.sql
@@ -1,9 +1,9 @@
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'neoAutoTest')
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'AutoTest_NM_27Mar')
 BEGIN
-    CREATE DATABASE neoAutoTest;
-    PRINT 'Database neoAutoTest created successfully.';
+    CREATE DATABASE AutoTest_NM_27Mar;
+    PRINT 'Database AutoTest_NM_27Mar created successfully.';
 END
 ELSE
 BEGIN
-    PRINT 'Database neoAutoTest already exists.';
+    PRINT 'Database AutoTest_NM_27Mar already exists.';
 END
