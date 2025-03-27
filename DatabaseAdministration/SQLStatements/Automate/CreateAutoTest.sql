d7addfc5cf9b79aa15e7fc4229a02bf043ffa430
NeoMotaung <neo.motaung@sambeconsulting.com>
2025-03-27 14:16:49 +0200

    Update CreateAutoTest.sql

--- DatabaseAdministration/SQLStatements/Automate/CreateAutoTest.sql
+++ DatabaseAdministration/SQLStatements/Automate/CreateAutoTest.sql
@@ -1,9 +1,9 @@
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'AutoTestDev_27Mar')
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'AutoTest_NM_27Mar')
 BEGIN
-    CREATE DATABASE AutoTestDev_27Mar;
-    PRINT 'Database AutoTestDev_27Mar created successfully.';
+    CREATE DATABASE AutoTest_NM_27Mar;
+    PRINT 'Database AutoTest_NM_27Mar created successfully.';
 END
 ELSE
 BEGIN
-    PRINT 'Database AutoTestDev_27Mar already exists.';
+    PRINT 'Database AutoTest_NM_27Mar already exists.';
 END
