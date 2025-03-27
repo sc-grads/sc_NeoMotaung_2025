22b4761a18619ed5fc168b12272aeca5171d9edd
NeoMotaung <neo.motaung@sambeconsulting.com>
2025-03-27 14:53:49 +0200

    Update InsertValues.sql

--- DatabaseAdministration/SQLStatements/Automate/InsertValues.sql
+++ DatabaseAdministration/SQLStatements/Automate/InsertValues.sql
@@ -1,14 +1,16 @@
 USE AutoTest_NM_27March
 GO
 
-/*CREATE PROCEDURE InsertUser
+DROP PROCEDURE IF EXISTS InsertUser;
+	
+CREATE PROCEDURE InsertUser
     @fName VARCHAR(50),
     @sName VARCHAR(50),
-	@Email VARCHAR(50)
+    @Email VARCHAR(50)
 AS
 BEGIN
     IF @Email IS NOT NULL AND EXISTS (
-        SELECT 1 FROM tUser WHERE Email = @Email AND fName = @fName AND Surname = @sName
+        SELECT 1 FROM tUser WHERE Email = @Email
     )
     BEGIN
 	RAISERROR('Customer with this email already exists.', 16, 1);
@@ -19,6 +21,7 @@ BEGIN
     VALUES (@fName, @sName, @Email);
 END;
 
+DELETE * FROM tUser;
 -- Execute the stored procedure with sample values
 EXEC InsertUser @fName = 'Leonardo', @sName = 'Smith', @Email = 'l.smith@gmail.com';
 
@@ -26,8 +29,8 @@ EXEC InsertUser @fName = 'Donatello', @sName = 'Doe', @Email = 'd.doe@gmail.com'
 
 EXEC InsertUser @fName = 'Michaelangelo', @sName = 'Johnson', @Email = 'm.Johnson@gmail.com';
 
-EXEC InsertUser @fName = 'Raphael', @sName = 'Johnson', @Email = 'r.johnson@gmail.com';*/
+EXEC InsertUser @fName = 'Raphael', @sName = 'Johnson', @Email = 'r.johnson@gmail.com';
 
-INSERT INTO tUser(fName, Surname, Email) VALUES ('Leonardo', 'Smith', 'l.smith@gmail.com');
+/*INSERT INTO tUser(fName, Surname, Email) VALUES ('Leonardo', 'Smith', 'l.smith@gmail.com');
 INSERT INTO tUser(fName, Surname, Email) VALUES ('Donnie', 'James', 'd.james@gmail.com');
-INSERT INTO tUser(fName, Surname, Email) VALUES ('Mickey', 'Roy', 'm.roy@gmail.com');
+INSERT INTO tUser(fName, Surname, Email) VALUES ('Mickey', 'Roy', 'm.roy@gmail.com');*/
