22b35836d70ee2096715703580c6cfdb58a3b152
NeoMotaung <neo.motaung@sambeconsulting.com>
2025-03-27 12:55:32 +0200

    Update TableUserMzu.sql

--- DatabaseAdministration/SQLStatements/Automate/TableUserMzu.sql
+++ DatabaseAdministration/SQLStatements/Automate/TableUserMzu.sql
@@ -1,16 +1,16 @@
 USE AutoTest_NM_27Mar; 
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'User_NM_27Mar' AND type = 'U')
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'tUser' AND type = 'U')
 BEGIN
-    CREATE TABLE User_NM_27Mar (
+    CREATE TABLE tUser (
         fName VARCHAR(50) NOT NULL,
         Surname VARCHAR(50) NOT NULL,
         Email VARCHAR(50) NOT NULL
     );
-    PRINT 'Table User_NM_27Mar created successfully.';
+    PRINT 'Table User created successfully.';
 END
 ELSE
 BEGIN
-    PRINT 'Table User_NM_27Mar already exists.';
+    PRINT 'Table User already exists.';
 END
