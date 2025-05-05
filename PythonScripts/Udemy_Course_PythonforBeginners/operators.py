26678ae5d7dda133f0dc9b838fa1df6009a7d96b
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-05-05 11:02:47 +0200

    Adding operators script

---
+++ PythonScripts/Udemy_Course_PythonforBeginners/operators.py
@@ -0,0 +1,40 @@
+a = 10
+b = 6
+
+#print(a / b) #division
+#print(a // b) #floor
+#print(a % b) #modulus
+
+#Order of operations and paranthesis
+#print(a + a * b)
+#print((a + a) * b)
+
+#Shortcut operations
+
+#print(a)
+a += 2
+a -= 2
+a *= 2
+a /= 2
+a **= 2
+#print(a)
+
+#Comparison Operators
+
+a = 12
+b = 10
+c = 3
+d = 14
+
+#print(a > b)
+#print(c > d)
+#print(a <= b)
+#print(c <= d)
+
+#print(a == c)
+#print(a != c)
+
+#Logical Operators
+print(a > b and c > d)
+print(a > b or c > d)
+print(not (a > b))
