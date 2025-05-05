62387d57090172c3479b7e3b3817945198c50067
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-05-05 11:13:20 +0200

    Simple adder project

---
+++ PythonProgramming/Udemy_Course_PythonforBeginners/simple_adder.py
@@ -0,0 +1,10 @@
+print("Welcome to the Simple Adder!")
+print("===================================================")
+
+num1 = input("Please enter the first number: ")
+num2 = input("Please enter the second number: ")
+
+print("=====================================================")
+
+sum = int(num1) + int(num2)
+print("The sum of the 2 numbers is", sum)
