81a2895dbb51aee79ec6e185e27e3f72f8677b44
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-05-05 09:52:42 +0200

    Added python scripts folder for scripts from the udemy course

---
+++ PythonScripts/Udemy_Course_PythonforBeginners/types.py
@@ -0,0 +1,33 @@
+#Datatypes
+
+#Numerical Types
+
+age = 22 #int
+money = 1100.53 #float
+
+#Boolean type
+#Values are either True or False
+#Must start with a capital letter T or F
+is_broke = True
+is_Happy = False
+#print(type(is_broke))
+
+#String Type
+
+phrase = "This is my new catch phrase"
+#print(type(phrase))
+
+#Sequence Types
+
+n_list = [1, 2, 3, 4, 5] #list
+coordinates = (1.5, 6) #tuple, 2-tuple in this case
+
+#Mapping Types
+
+users = {"Mario" : 1, "Luigi" : 2} #dictionary
+
+#Set Types
+
+raffle = {1, 9, 10, 12, 50} #set
+
+frozen = frozenset({1,3,5}) #frozen set, doesnt allow changes
