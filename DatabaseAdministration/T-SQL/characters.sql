530e82a82f073a72edfb3b68d910a5d8b9b05fbd
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-05-19 15:48:17 +0200

    Strings

---
+++ DatabaseAdministration/T-SQL/characters.sql
@@ -0,0 +1,10 @@
+﻿--char - ASCII - 1 Byte
+--varchar - ASCII - 1 Byte
+--nchar - UNICODE - 2 Bytes
+--nvarchar - UNICODE - 2 Bytes
+
+DECLARE @myChars as nvarchar(10)
+
+SET @myChars = N'helloǽ'
+
+SELECT @myChars as MyString, LEN(@myChars) as myLength, DATALENGTH(@myChars) as myDataLength -- +2 for varchar
