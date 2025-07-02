1b609ff9e9d203266b173f6b92c6629f107fe7f6
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-07-02 08:19:22 +0200

    Data migration functionality

---
+++ RESTAPIProj/decorator.py
@@ -0,0 +1,14 @@
+user = {"username": "jose", "access level": "guest"}
+
+def get_admin_password():
+    return ("1234")
+
+def make_secure(func):
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+    return secure_function()
+
+get_admin_password = make_secure(get_admin_password)
+
+print(get_admin_password())
