64a5e1e38bf114e5a61a08df3147aed89d1c1584
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-08-12 10:50:50 +0200

    C# program files

---
+++ CSharpDevelopment/logic_operators.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRainy = true;
+            bool hasUmbrella = true;
+            if (!isRainy || hasUmbrella)
+            {
+                Console.WriteLine("I'm protected");
+            }
+            Console.WriteLine("Ay Ok!");
+        }
+    }
+}
---
+++ CSharpDevelopment/numerical_dt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_AdditionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numOne;
+            double numTwo;
+            
+            Console.WriteLine("Enter the first number: ");
+            numOne = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            numTwo = double.Parse(Console.ReadLine());
+
+            double sum = numOne + numTwo;
+            Console.WriteLine($"The sum of the two numbers: {sum}");
+        }
+    }
+}
