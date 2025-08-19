627cd32e765a857ed7543d58cc3587055553f82e
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-08-19 10:40:00 +0200

    OOP and inheritance

---
+++ CSharpDevelopment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizAppOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question(
+                    "What is the capital of South Africa?",
+                    new string[]{"Paris", "Durban", "Johannesburg", "Pretoria"},
+                    3
+                ),
+                new Question(
+                    "How many sides does a triangle have?",
+                    new string[]{"4", "3", "7", "5"},
+                    1
+                ),
+                new Question(
+                    "What is 3*2?",
+                    new string[]{"6", "3", "5", "10"},
+                    0
+                )
+            };
+            
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.StartQuiz();
+        }
+    }
+}
---
+++ CSharpDevelopment/override.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Make noise...");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            dog.MakeSound();
+            //cat.MakeSound();
+
+        }
+    }
+}
