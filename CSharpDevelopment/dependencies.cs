02d71c8d420e2bde7d4e696bff7fc57bf826a651
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-08-20 10:04:12 +0200

    Polymorphism, interfaces and structs

---
+++ CSharpDevelopment/dependencies.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDependency
+{
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Hammering Nails");
+        }
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing planks");
+        }
+    }
+
+    public class Builder
+    {
+        private Hammer _hammer;
+        private Saw _saw;
+
+        public Builder()
+        {
+            _hammer = new Hammer();
+            _saw = new Saw();
+
+        }
+
+        public void buildHouse()
+        {
+            _hammer.Use();
+            _saw.Use();
+            Console.WriteLine("House built");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CSharpDevelopment/more_structs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsApp
+{
+    public struct Point
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Point: ({X},{Y})");
+        }
+
+        public double GetDistance(Point other)
+        {
+            double dx = other.X - X;
+            double dy = other.Y - Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(5, 10);
+            p.Display();
+
+            Point p2 = new Point(10, 15);
+;
+            p2.Display();
+
+            double distance = p.GetDistance(p2);
+            Console.WriteLine($"Distance between p and p2: {distance:F2}");
+
+            Point p3 = p;
+            //p3.Display();
+        }
+    }
+}
