ec12c0eb1bc54b056b17661eb70d150b586ae944
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-08-22 10:23:46 +0200

    WPF Tutorial

---
+++ CSharpDevelopment/WPFMore/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person
+        {
+            Name = "Jannick",
+            Age = 10
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World")
+;        }
+    }
+}
