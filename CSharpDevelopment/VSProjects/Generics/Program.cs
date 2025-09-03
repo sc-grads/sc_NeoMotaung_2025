c611ca3030c684fc744ec8c976c0bae85e0b963a
Neo Motaung <neo.motaung@sambeconsulting.com>
2025-09-03 14:24:20 +0200

    Creating a generic class

---
+++ CSharpDevelopment/VSProjects/Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> intBox = new Box<int>();
+            intBox.Content = 123;
+
+            Box<string> strBox = new Box<string>();
+            strBox.Content = "Hello Generics";
+        }
+    }
+
+    class Box<T>
+    {
+        public T Content { get; set; }
+
+        public string Log()
+        {
+            return $"Box contains: {Content}";
+        }
+    }
+}
